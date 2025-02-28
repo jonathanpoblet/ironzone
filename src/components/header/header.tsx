9147ec3c19306523071b0a7ca261c8081b3e8650
unknown <jonathanpoblet228@gmail.com>
2025-02-28 10:28:58 -0300

    Update | Image component in header/header.tsx

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
+import Image from 'next/image';
 import './header.css';
 
 export default function Header() {
   return (
     <header className='flex justify-between items-center px-8 bg-white shadow-md w-full'>
       <div className='flex items-center h-full'>
-        <a href='/'>
-          <img src='/iron-logo.png' alt='Logo' className='h-25' />
-        </a>
+        <Image width='100' height='100' src='/iron-logo.png' alt='logo' />
       </div>
 
       <div className='flex items-center h-full'>
